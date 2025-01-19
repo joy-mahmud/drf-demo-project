6e4cae0f913b0d587ff0bafd549185c6038f7bf4
joy-mahmud <joymahmudiucse@gmail.com>
2025-01-19 23:45:56 +0600

    modelserializer

--- books/serializers.py
+++ books/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-
+from .models import Book
 class BookSerializer(serializers.Serializer):
     title=serializers.CharField(max_length=200)
     author=serializers.CharField(max_length=200)
+    
+class BookModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields="__all__"
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import BookSerializer
+from .serializers import BookSerializer,BookModelSerializer
+from .models import Book
 # Create your views here.
 class BookListView(APIView):
     def get(self,request):
@@ -11,3 +12,17 @@ class BookListView(APIView):
         ]
         serializer = BookSerializer(books,many=True)
         return Response(serializer.data)
+
+class BookListModelView(APIView):
+    def get(self,request):
+        books = Book.objects.all()
+        serializer=BookModelSerializer(books,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer=BookModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+        
